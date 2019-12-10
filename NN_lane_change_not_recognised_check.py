2cad668d0059e8217476ec524abcdda82762a3dd
Haoyu <arhyrhy185@gmail.com>
2019-12-10 23:38:05 +0100

    add check code to validate sample direct from recording

--- NN_lane_change_not_recognised_check.py
+++ NN_lane_change_not_recognised_check.py
@@ -1,35 +1,57 @@
 import os
+# import tensorflow as tf
+from keras.models import load_model
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 if __name__ == "__main__":
 
-    path = r'..\preprocessed_data\processed_data\csv\second\0_cv_ego.csv'
+    path = r'processed_data\csv\0_cv_ego_test.csv'
     # Walk through every ego data
 
-
     # Depend on number of features and length of each example
+    start_frame = 18500
     window_size = 500
 
-    columns_name = ['speed', 'acc_x', 'acc_y']
+    columns_name = ['speed', 'acc_x', 'acc_y', 'steering_ang']
+    trained_model = load_model(r'NN_data\my_model.h5')
 
-    trained_model = load_model(r'NN_data\model_all_2.h5')
+    # columns_name = ['speed', 'acc_x', 'acc_y']
+    # trained_model = load_model(r'NN_data\model_without_steering_ang_6.h5')
+
+    mean_and_variance = np.load(r'NN_data\mean_std.npy')
+
+    df = pd.read_csv(path)
+    sample = df[columns_name][start_frame:start_frame+window_size]
+
+    # normalized_sample = (sample - mean_and_variance[0, :-1])/mean_and_variance[1, :-1]
+    normalized_sample = (sample - mean_and_variance[0, :]) / mean_and_variance[1, :]
+
 
     # Following visualize the label in on sample
-    y_test = trained_model.predict(np.expand_dims(X[num, :, :], axis=0))
+    y_predicted = trained_model.predict(np.expand_dims(normalized_sample, axis=0))
 
-    X = np.concatenate([x for x in X_list], axis=0)
-    Y = np.concatenate([y for y in Y_list], axis=0)
+    fig = plt.figure()
+    axes_1 = fig.add_subplot(3, 1, 1)
+    axes_1.plot(y_predicted[0, :, 0])
+    axes_1.set_title('Free driving label - Predicted')
+    axes_1.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '%g' % (x * 2)))
+    axes_1.set_ylim([-0.1, 1.1])
+    # axes_1.set_autoscaley_on(False)
 
-    X, mean, std = normalization(X)
-    # Reshape X and Y to fit the input of NN
-    X = np.reshape(X, (int(X.shape[0]/window_size), window_size, len(columns_name)))
-    Y = np.reshape(Y, (int(Y.shape[0]/new_y_length), new_y_length, class_num))
-    # Shuffle the X and Y
-    p = np.random.permutation(X.shape[0])
-    X = X[p, :, :]
-    Y = Y[p, :, :]
+    axes_2 = fig.add_subplot(3, 1, 2)
+    axes_2.plot(y_predicted[0, :, 1])
+    axes_2.set_title('Left lane change label - Predicted')
+    axes_2.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '%g' % (x * 2)))
+    axes_2.set_ylim([-0.1, 1.1])
 
-    mean_std = np.reshape(np.concatenate((mean, std), axis=0), (2, len(columns_name)))
+    axes_3 = fig.add_subplot(3, 1, 3)
+    axes_3.plot(y_predicted[0, :, 2])
+    axes_3.set_title('Right lane change label - Predicted')
+    axes_3.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '%g' % (x * 2)))
+    axes_3.set_ylim([-0.1, 1.1])
+    plt.tight_layout()
+    plt.show()
 
-    np.save('{}\\X_without_steering_ang.npy'.format(path), X)
-    np.save('{}\\Y_without_steering_ang.npy'.format(path), Y)
-    np.save('{}\\mean_std_without_steering_ang.npy'.format(path), mean_std)
